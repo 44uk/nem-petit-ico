c3bc1d45e04250ef3c1d79bc55c2de0de62d44d4
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-10-19 11:01:27 +0000

    most rewrited

--- _tokensale.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call npm run --silent tokensale
-pause
---
+++ bin/sendmosaic.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const applicant = require('../lib/applicant.js');
+const fs = require('fs-extra');
+const helper = require('../lib/helper.js');
+const logger = helper.logger('bin/sendmosaics');
+
+const DRYRUN = process.env.MOSAICSALE_DRYRUN || 1;
+const PKEY = process.env.MOSAICSALE_PKEY;
+const MOSAIC_FQN = process.env.MOSAICSALE_MOSAIC_FQN;
+
+const MIN_TIME = Date.parse(process.env.MOSAICSALE_MIN_TIME);
+const MAX_TIME = Date.parse(process.env.MOSAICSALE_MAX_TIME);
+const MIN_NEMTIME = helper.NEMTimestamp(MIN_TIME);
+const MAX_NEMTIME = helper.NEMTimestamp(MAX_TIME);
+
+const MESSAGE = process.env.MOSAICSALE_MESSAGE || '';
+
+logger.info('# Settings');
+logger.info(`DRYRUN = ${DRYRUN}`);
+logger.info(`MOSAIC_FQN = ${MOSAIC_FQN}`);
+logger.info(`MIN_TIME = ${process.env.MOSAICSALE_MIN_TIME}`);
+logger.info(`MAX_TIME = ${process.env.MOSAICSALE_MAX_TIME}`);
+
+logger.info(`MESSAGE = ${MESSAGE}`);
+
+function loadCandidates() {
+  return fs.readFile('tmp/applicants.json')
+  .then(JSON.parse)
+  .then(data => { return data.candidates; })
+  ;
+}
+
+function main() {
+  let now = new Date().getTime();
+  if(now < MIN_TIME) {
+    logger.info('# before begin ICO');
+    return;
+  }
+  logger.info('# start sending');
+
+  loadCandidates()
+  .then(candidates => {
+    if(DRYRUN != 0) {
+      logger.info(candidates);
+      console.log(candidates);
+      throw new Error(`Enabled DRYRUN! No transactions announced.`);
+    }
+    if(candidates.length === 0) {
+      throw new Error(`No candidates!`);
+    }
+    return candidates;
+  })
+  .then(applicant.sendMosaics(MOSAIC_FQN, MESSAGE, PKEY))
+  .then(res=> {
+    console.log(res);
+  })
+  .catch(err => {
+    logger.error(err);
+  })
+  ;
+}
+
+if (require.main === module) {
+  logger.debug('Called directly.');
+  main();
+} else {
+  logger.debug('Required as a module.');
+  module.exports = main;
+}
