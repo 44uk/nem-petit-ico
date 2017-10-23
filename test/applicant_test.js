ca1b0412252e46d8617fce8507b33f92b8f20383
Yoshiyuki Ieyama <yukku0423@gmail.com>
2017-10-23 08:10:12 +0000

    create fixtures

--- test/applicant_test.js
+++ test/applicant_test.js
@@ -15,53 +15,7 @@ describe('Applicant', () => {
 
     const scope = nock('http://bigalice2.nem.ninja:7890')
       .get(`/account/transfers/incoming?address=${addr}`)
-      .reply(200, { data: [
-        {
-          meta: {
-            hash: { data: "79e56ebcc7bf067aaeeba71e6563d7309cece6664b7b7f046712668aefa05439" },
-          },
-          transaction: {
-            timeStamp: 86400 - 386 + 1,
-            amount: 1000000,
-            type: 257,
-            version: -1744830463,
-            signer: "08d5d8f415dd5facbc45ede9e5c97ab514c3d999ebe5d387552fdbf360cd21be"
-          }
-        },
-        {
-          meta: {
-            hash: { data: "79e56ebcc7bf067aaeeba71e6563d7309cece6664b7b7f046712668aefa05439" },
-          },
-          transaction: {
-            timeStamp: 86400 - 386,
-            amount: 1000000,
-            type: 257,
-            version: -1744830463,
-            signer: "08d5d8f415dd5facbc45ede9e5c97ab514c3d999ebe5d387552fdbf360cd21be"
-          }
-        },
-        {
-          meta: {
-            innerHash: {},
-            id: 135806,
-            id: 999999,
-            hash: { data: "79e56ebcc7bf067aaeeba71e6563d7309cece6664b7b7f046712668aefa05439" },
-            height: 1160571
-          },
-          transaction: {
-            timeStamp: 0,
-            amount: 1000000,
-            signature: "f913fb6f7dbb263e6720cc791887d2a3a5fa0b81f31239cecfa0cc677205e41eb698b601915bc7cc0a6bbb3d9099b5e862f276681a859a12a70313a815f02609",
-            fee: 50000,
-            recipient: "TBX7B37ZRLNISNIY7ZMLS7DWSMHW2T5733LOUP5P",
-            type: 257,
-            deadline: 3600,
-            message: {},
-            version: -1744830463,
-            signer: "08d5d8f415dd5facbc45ede9e5c97ab514c3d999ebe5d387552fdbf360cd21be"
-          }
-        }
-      ]})
+      .reply(200, require('./fixtures/incoming_applicants.json'))
     ;
 
     it('return a tx, summary received 2 xem', () => {
@@ -85,51 +39,7 @@ describe('Applicant', () => {
 
     const scope = nock('http://bigalice2.nem.ninja:7890')
       .get(`/account/transfers/outgoing?address=${addr}`)
-      .reply(200, { data: [
-        {
-          meta: {
-            innerHash: {},
-            id: 135820,
-            hash: { data: "17b7a1efcb4c89dfaa77ac689e56e6d7bca7532675bf1369c90211222db960ed" },
-            height: 1161152
-          },
-          transaction: {
-            timeStamp: 0,
-            amount: 1000000,
-            signature: "5d55988c3320b85e2ac3e78cc878c1936ab9bab39298cac3536c0126b7bc5cd0b07a647aff9cdf1e48f9acf576638331a178798f42c1ade21d561fe7deb13b0f",
-            fee: 50000,
-            recipient: "TAWKJTUP4DWKLDKKS534TYP6G324CBNMXKBA4X7B",
-            mosaics: [
-              { quantity: 1, mosaicId: { namespaceId: "y4uk", name: "gift" } }
-            ],
-            type: 257,
-            deadline: 80408961,
-            message: {},
-            version: -1744830462,
-            signer: "cc63b4dcdec745417043c3fa0992ec3a1695461a26d90264744648abbd5caa0d"
-          }
-        },
-        // {
-        //   meta: {
-        //     hash: { data: "17b7a1efcb4c89dfaa77ac689e56e6d7bca7532675bf1369c90211222db960ed" }
-        //   },
-        //   transaction: {
-        //     timeStamp: 86400 - 386 - 80000,
-        //     amount: 1000000,
-        //     signature: "5d55988c3320b85e2ac3e78cc878c1936ab9bab39298cac3536c0126b7bc5cd0b07a647aff9cdf1e48f9acf576638331a178798f42c1ade21d561fe7deb13b0f",
-        //     fee: 50000,
-        //     recipient: "TAWKJTUP4DWKLDKKS534TYP6G324CBNMXKBA4X7B",
-        //     mosaics: [
-        //       { quantity: 1, mosaicId: { namespaceId: "y4uk", name: "gift" } }
-        //     ],
-        //     type: 257,
-        //     deadline: 80408961,
-        //     message: {},
-        //     version: -1744830462,
-        //     signer: "cc63b4dcdec745417043c3fa0992ec3a1695461a26d90264744648abbd5caa0d"
-        //   }
-        // }
-      ]})
+      .reply(200, require('./fixtures/outgoing_applicants.json'))
     ;
 
     it('return a tx, sent a mosaic', () => {
@@ -153,28 +63,7 @@ describe('Applicant', () => {
 
     const scope = nock('http://bigalice2.nem.ninja:7890')
       .get(`/account/unconfirmedTransactions?address=${addr}`)
-      .reply(200, {
-        data: [
-          {
-            meta: { data: null },
-            transaction: {
-              timeStamp: 0,
-              amount: 1000000,
-              signature: "5d55988c3320b85e2ac3e78cc878c1936ab9bab39298cac3536c0126b7bc5cd0b07a647aff9cdf1e48f9acf576638331a178798f42c1ade21d561fe7deb13b0f",
-              fee: 50000,
-              recipient: "TAWKJTUP4DWKLDKKS534TYP6G324CBNMXKBA4X7B",
-              mosaics: [
-                { quantity: 1, mosaicId: { namespaceId: "y4uk", name: "gift" } }
-              ],
-              type: 257,
-              deadline: 80408961,
-              message: {},
-              version: -1744830462,
-              signer: "cc63b4dcdec745417043c3fa0992ec3a1695461a26d90264744648abbd5caa0d"
-            }
-          }
-        ]
-      })
+      .reply(200, require('./fixtures/unconfirmed_applicants.json'))
     ;
 
     it('return a tx, sent a mosaic', () => {
